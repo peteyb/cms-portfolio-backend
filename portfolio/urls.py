4aedd6c4f9112f61fe5902e85ba7a5460cdf1731
Peter Bryant <peter.bryant@thekeysupport.com>
2020-03-09 20:10:10 +0000

    no nested p lists

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -32,6 +32,7 @@ if settings.DEBUG:
     # urlpatterns += [
     #     url(r'^__debug__/', include(debug_toolbar.urls)),
     # ]
+    urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
 
